5a50f3beda61316375d64a3eb3a13181385accfe
Aryan-040 <singharyan432002@gmail.com>
2025-11-20 16:55:25 +0530

    implemented like comment feature

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,6 +1,7 @@
 import {isValidObjectId} from "mongoose"
 import {Like} from "../models/like.model.js"
 import {Video} from "../models/video.model.js"
+import {Comment} from "../models/comment.model.js"
 import ApiError from "../utils/apiError.js"
 import ApiResponse from "../utils/apiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
@@ -38,7 +39,42 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         .status(200)
         .json(new ApiResponse(200, {liked: true}, "Video liked successfully"))
 })
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const {commentId} = req.params
+    
+    if (!commentId || !isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID")
+    }
+
+    const comment = await Comment.findById(commentId)
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+    const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    if (existingLike) {
+        await existingLike.deleteOne()
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {liked: false}, "Comment like removed"))
+    }
+
+    await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {liked: true}, "Comment liked successfully"))
+})
 
 
 
-export {toggleVideoLike}
+export {toggleVideoLike,
+    toggleCommentLike
+}
